079dc610b3ccef6e08fa02f963639b79d5bdd29b
Timothy Adamcik <tadamcik0425@gmail.com>
2020-03-27 13:51:03 -0400

    first commit

---
+++ FanGo/Controller/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  FanGo
+//
+//  Created by Timothy Adamcik on 3/27/20.
+//  Copyright © 2020 Timothy Adamcik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapViewController: ViewController, MKMapViewDelegate {
+    
+}
