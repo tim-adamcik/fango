b750b132e707bd081fdbddcd5f3b11f74347f9d6
Timothy Adamcik <tadamcik0425@gmail.com>
2020-04-01 11:19:20 -0400

    reconnected table and mapview

--- FanGo/Controller/StadiumListViewController.swift
+++ FanGo/Controller/StadiumListViewController.swift
@@ -9,8 +9,27 @@
 import Foundation
 import UIKit
 
-class StadiumListViewController: UIViewController {
+class StadiumListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var stadiumListTableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stadiumListTableView.delegate = self
+        stadiumListTableView.dataSource = self
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as! TableViewCell
+        cell.label.text = "hello"
+        
+        return cell
+    }
     
 }
---
+++ FanGo/View/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  FanGo
+//
+//  Created by Timothy Adamcik on 4/1/20.
+//  Copyright © 2020 Timothy Adamcik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
